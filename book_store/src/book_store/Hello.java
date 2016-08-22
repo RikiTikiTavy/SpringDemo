828619f3efe1c537b8f074bff63f2cbd3605f404
Morra <theredballpoint.com>
2016-08-23 00:16:21 +0400

    added a project

---
+++ book_store/src/book_store/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package book_store;
+
+/**
+ * @author пользователь
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
