3ffd786de2f0cf34143883ad1e65eb73463056c4
Morra <theredballpoint.com>
2016-10-03 02:57:12 +0400

    New Spring Project

---
+++ SpringDemo/src/ru/roman/javabrains/DrawlingApp.java
@@ -0,0 +1,19 @@
+package ru.roman.javabrains;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawlingApp {
+
+	public static void main(String[] args) {
+		// Triangle triangle = new Triangle();
+		// BeanFactory factory = new XmlBeanFactory(new
+		// FileSystemResource("spring.xml"));
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
+		triangle.draw();
+
+	}
+
+}
